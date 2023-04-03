1b0937f51873ba196a9800598b30081bc3a4261e
crnunl <129716630+crnunl@users.noreply.github.com>
2023-04-03 15:38:18 +0300

    Update SRV_Channel.cpp

--- libraries/SRV_Channel/SRV_Channel.cpp
+++ libraries/SRV_Channel/SRV_Channel.cpp
@@ -159,9 +159,7 @@ void SRV_Channel::set_output_norm(float value)
     }
 }
 
-void set_output_min(uint16_t pwm) {
-        servo_min.set(pwm);
-  }
+
 
 // set angular range of scaled output
 void SRV_Channel::set_angle(int16_t angle)
